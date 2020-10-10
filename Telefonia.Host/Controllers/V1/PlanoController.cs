e177ba8540cf2d00ed6a2eb21ca8f533a29fad62
Vinicius Anete <viniciusanete@gmail.com>
2020-10-10 16:04:33 -0300

    Controller de Plano

---
+++ Telefonia.Host/Controllers/V1/PlanoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telefonia.Domain.Plano;
+
+namespace Telefonia.Host.Controllers.V1
+{
+    [Route("v1/plano")]
+    public class PlanoController : ControllerBase
+    {
+        private readonly IPlanoService _planoService;
+
+        public PlanoController(IPlanoService planoService)
+        {
+            _planoService = planoService ?? throw new ArgumentNullException(nameof(planoService));
+        }
+    }
+}
