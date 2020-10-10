ae4d72669af464cbcc639cd0edf5ca91d7fe025c
Vinicius Anete <viniciusanete@gmail.com>
2020-10-10 15:15:16 -0300

    - Adicionando Health check (API e Banco de Dados) - Configuração da API (Startup)

--- Telefonia.Host/Startup.cs
+++ Telefonia.Host/Startup.cs
@@ -2,12 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Telefonia.Host.Infrastructure.ExtensionMethods;
 
 namespace Telefonia.Host
 {
@@ -23,13 +27,31 @@ namespace Telefonia.Host
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<Infrastructure.Data.Config.IDbSettings, Infrastructure.Data.Config.DbSettings>((o) => new Infrastructure.Data.Config.DbSettings
+            services.AddSingleton<Telefonia.Infrastructure.Data.Config.IDbSettings, Telefonia.Infrastructure.Data.Config.DbSettings>((o) => new Telefonia.Infrastructure.Data.Config.DbSettings
             {
                 ConnectionString = Configuration.GetValue<string>("ConnectionString")
             });
+
+            services.AddDomain();
+            services.AddControllers();
+
+            services.AddHealthChecks()
+                .AddCheck("self", () => HealthCheckResult.Healthy())
+                .AddCheck("DataBase", new Infrastructure.HealthCheck.SqliteConnectionHealthCheck(Configuration.GetValue<string>("ConnectionString")),
+                                                   HealthStatus.Unhealthy,
+                                                   new string[] { "DataBase" });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,14 +59,23 @@ namespace Telefonia.Host
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHealthChecks("/hc", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks("/liveness", new HealthCheckOptions
+            {
+                Predicate = r => r.Name.Contains("self")
+            });
+
+            app.UseCors("CorsPolicy");
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
